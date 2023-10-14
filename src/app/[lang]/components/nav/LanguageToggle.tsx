fc286f23893eccd8a99d8fc0e2159d932c1f2d80
garethwilliams90 <wgareth90@gmail.com>
2023-10-14 10:11:34 +0100

    removed tooltip

--- src/app/[lang]/components/nav/LanguageToggle.tsx
+++ src/app/[lang]/components/nav/LanguageToggle.tsx
@@ -5,40 +5,25 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/app/[lang]/components/ui/dropdown-menu"
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
 
 import LocaleSwitcher from "./LocaleSwitcher"
 
 export function LanguageToggle() {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <Languages />
-                <span className="sr-only">Change Language</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              align="end"
-              className="mx-2 p-4 items-center justify-center"
-            >
-              <LocaleSwitcher />
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Change Language</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Languages />
+          <span className="sr-only">Change Language</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        className="mx-2 p-4 items-center justify-center"
+      >
+        <LocaleSwitcher />
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
--- src/app/[lang]/components/nav/ModeToggle.tsx
+++ src/app/[lang]/components/nav/ModeToggle.tsx
@@ -10,56 +10,40 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/app/[lang]/components/ui/dropdown-menu"
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../ui/tooltip"
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          {" "}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="mx-2">
-              <DropdownMenuItem onClick={() => setTheme("light")}>
-                <div className="w-full flex flex-row items-center justify-between">
-                  Light
-                  <Sun className="w-4 h-4" />
-                </div>
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("dark")}>
-                <div className="w-full flex flex-row items-center justify-between">
-                  Dark
-                  <Moon className="w-4 h-4" />
-                </div>
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("system")}>
-                <div className="w-full flex flex-row items-center justify-between">
-                  System
-                  <Computer className="w-4 h-4" />
-                </div>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Change theme</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="mx-2">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <div className="w-full flex flex-row items-center justify-between">
+            Light
+            <Sun className="w-4 h-4" />
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <div className="w-full flex flex-row items-center justify-between">
+            Dark
+            <Moon className="w-4 h-4" />
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <div className="w-full flex flex-row items-center justify-between">
+            System
+            <Computer className="w-4 h-4" />
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
